6d3a6acd51ed6dc93ca1ae85ce1f90f2d525ad50
fl4cko11 <khomy4kovvv@gmail.com>
2025-03-21 15:00:10 +0300

    delete tests dir

--- CPP_tests/DB_Connection_TEST.cpp
+++
@@ -1,8 +0,0 @@
-#include "/home/vladh/git-repos/Music_Player_MIPT/CPP_classes/MediaPlayerController.hpp"
-
-int main() {
-    Track specialTrack{"praise", "asap", 0, 1, 0};
-
-    std::cout << specialTrack.GetAudioPathFromDb() << std::endl;
-    return 0;
-}
